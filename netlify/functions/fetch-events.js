f6bfb07ae899f1d5c713383d394915c21a3816b7
James T. Harding <59349643+empowermint@users.noreply.github.com>
2023-01-03 10:23:21 +0000

    Implement Discord link

--- netlify/functions/fetch-events.js
+++ netlify/functions/fetch-events.js
@@ -1,9 +1,28 @@
 exports.handler = async (event, context) => {
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      testString: "This is the test string from functions/fetch-events"
+  try {
+    const response = await fetch(`https://discord.com/api/v10/guilds/${process.env.DISCORD_GUILD_ID}/scheduled-events`, { // Docs: https://discord.com/developers/docs/resources/guild-scheduled-event#list-scheduled-events-for-guild
+      headers: {
+        'Authorization': `Bot ${process.env.DISCORD_BOT_TOKEN}`,
+        'Content-Type': 'application/json; charset=UTF-8',
+        'User-Agent': 'DiscordBot (https://edinburghindiegamers.com, 1.0.0)'
+      }
     })
+    const data = await response.json()
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        events: data
+      })
+    }
+  } catch (e) {
+    console.error(e)
+    
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        error: e
+      })
+    }
   }
 }
--- pages/events.js
+++ pages/events.js
@@ -10,21 +10,30 @@ export default function Events(props) {
   return (
     <Layout meta={meta}>
       <EventsIntro />
-      <h4>Discord Events List</h4>
-      <p>{props.testString}</p>
+      <h2>Upcoming Events</h2>
+      {console.dir(props)}
+      <ul>
+        {props.events.map((event) =>
+          <li key={event.id}>
+            <h3>{event.name}</h3>
+            <p>{event.scheduled_start_time} to {event.scheduled_end_time}</p>
+            <p>{event.description}</p>
+          </li>
+        )}
+      </ul>
     </Layout>
   )
 }
 
 export async function getStaticProps(context) {
-  const response = await fetch(`${process.env.URL}/.netlify/functions/fetch-events`)
+  const response = await fetch('http://localhost:8888/.netlify/functions/fetch-events')
   if (!response.ok) console.error("Fetch response status: " + response.status)
   const data = await response.json()
 
   return {
     revalidate: 86400,
     props: {
-      testString: data.testString
+      events: data.events
     }
   }
 }
