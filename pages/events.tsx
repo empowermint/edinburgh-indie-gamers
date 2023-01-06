5e7430d3c447a3016e7a51f0387be9eafb7704b6
James T. Harding <59349643+empowermint@users.noreply.github.com>
2023-01-06 22:40:26 +0000

    Add Discord types

--- pages/events.tsx
+++ pages/events.tsx
@@ -2,8 +2,16 @@ import Layout from "../components/Layout"
 import DisplayDate from "../components/DisplayEventDate"
 import EventsIntro from "../components/EventsIntro.mdx"
 
-export default function Events({ events }) {
+import {  GetServerSideProps } from 'next';
+interface Event {
+  id: Number,
+  name: String,
+  scheduled_start_time: String,
+  scheduled_end_time: String,
+  description: String
+}
 
+export default function Events({ events }: { events: Event[]}) {
   const meta = {
     title: "Events",
     description: "Edinburgh Indie Gamers' vibrant event calendar"
@@ -15,7 +23,7 @@ export default function Events({ events }) {
       <h2 className="event-title">Upcoming Events</h2>
       <ul className="event-list">
         {events.map((event) =>
-          <li key={event.id}>
+          <li key={event.id.toString()}>
             <h3>{event.name}</h3>
             <DisplayDate start={event.scheduled_start_time} end={event.scheduled_end_time} />
             <p className="event-description">{event.description}</p>
@@ -26,7 +34,7 @@ export default function Events({ events }) {
   )
 }
 
-export async function getServerSideProps(context) {
+export const getServerSideProps: GetServerSideProps = async (context) => {
   try {
     const response = await fetch(`https://discord.com/api/v10/guilds/${process.env.DISCORD_GUILD_ID}/scheduled-events`, { // Docs: https://discord.com/developers/docs/resources/guild-scheduled-event#list-scheduled-events-for-guild
       headers: {
@@ -35,7 +43,7 @@ export async function getServerSideProps(context) {
         'User-Agent': 'DiscordBot (https://edinburghindiegamers.com, 1.0.0)'
       }
     })
-    const data = await response.json()
+    const data: Array<Event> = await response.json()
 
     return {
       props: {
