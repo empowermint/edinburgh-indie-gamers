0129f4f8efc01dc6e3c3e9b34b3ce8ff601ddc0d
James T. Harding <59349643+empowermint@users.noreply.github.com>
2023-03-05 11:01:35 +0000

    Refactor

--- pages/events.tsx
+++ pages/events.tsx
@@ -1,8 +1,8 @@
 import Layout from "../components/Layout"
 import DisplayDate from "../components/DisplayEventDate"
 import EventsIntro from "../components/EventsIntro.mdx"
+import {  GetServerSideProps } from "next"
 
-import {  GetServerSideProps } from 'next';
 interface Event {
   id: Number,
   name: String,
@@ -22,12 +22,14 @@ export default function Events({ events }: { events: Event[]}) {
       <EventsIntro />
       <h2 className="event-title">Upcoming Events</h2>
       <ul className="event-list">
-        {events.map((event) =>
-          <li key={event.id.toString()}>
-            <h3>{event.name}</h3>
-            <DisplayDate start={event.scheduled_start_time} end={event.scheduled_end_time} />
-            <p className="event-description">{event.description}</p>
-          </li>
+        {events.map((event) => {
+          return (
+            <li key={event.id.toString()}>
+              <h3>{event.name}</h3>
+              <DisplayDate start={event.scheduled_start_time} end={event.scheduled_end_time} />
+              <p className="event-description">{event.description}</p>
+            </li>
+          )}
         )}
       </ul>
     </Layout>
@@ -38,9 +40,9 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   try {
     const response = await fetch(`https://discord.com/api/v10/guilds/${process.env.DISCORD_GUILD_ID}/scheduled-events`, { // Docs: https://discord.com/developers/docs/resources/guild-scheduled-event#list-scheduled-events-for-guild
       headers: {
-        'Authorization': `Bot ${process.env.DISCORD_BOT_TOKEN}`,
-        'Content-Type': 'application/json; charset=UTF-8',
-        'User-Agent': 'DiscordBot (https://edinburghindiegamers.com, 1.0.0)'
+        "Authorization": `Bot ${process.env.DISCORD_BOT_TOKEN}`,
+        "Content-Type": "application/json; charset=UTF-8",
+        "User-Agent": "DiscordBot (https://edinburghindiegamers.com, 1.0.0)"
       }
     })
     const data: Array<Event> = await response.json()
